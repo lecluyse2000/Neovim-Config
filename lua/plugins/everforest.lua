7076e272ae9610a3e8630934ac66b182dfee568b
Caden LeCluyse <lecluyse20@gmail.com>
2024-03-10 05:56:10 -0500

    Fixed weird formatting in everforest lua

--- lua/plugins/everforest.lua
+++ lua/plugins/everforest.lua
@@ -1,13 +1,13 @@
 return { 
-	"sainnhe/everforest",
-	lazy = false,
-	priority = 999,
-	config = function()
+   "sainnhe/everforest",
+   lazy = false,
+   priority = 999,
+   config = function()
       vim.cmd('set background=dark')
       vim.g.everforest_background = 'hard'
       vim.g.everforest_disable_italic_comment = 1
       vim.g.everforest_ui_contrast = 'high'
       vim.g.everforest_better_performance = 1
-		vim.cmd('colorscheme everforest')
-	end
+      vim.cmd('colorscheme everforest')
+   end
 }
