a971ef0b403fb54814125e77861e889de3f006d7
lecluyse2000 <lecluyse20@gmail.com>
2024-03-17 16:31:32 -0500

    Added a new keybind
    Made it so pressing escape in terminal mode changes it to normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,4 @@ local keymap = vim.keymap
 -- Directory Navigation
 keymap.set("n", "<leader>m", ":NvimTreeFocus<CR>", { noremap = true, silent = true})
 keymap.set("n", "<leader>f", ":NvimTreeToggle<CR>", { noremap = true, silent = true})
+keymap.set("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
