6700304943119c375952a2a1a112aafea14cca00
Caden LeCluyse <lecluyse20@gmail.com>
2024-04-08 02:43:41 -0500

    Finally set up whichkey
    LSP soon

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,6 +3,7 @@ local keymap = vim.keymap
 -- Directory Navigation
 keymap.set("n", "<leader>bn", ":bnext<CR>", {noremap = true, silent = true})
 keymap.set("n", "<leader>bp", ":bprevious<CR>", {noremap = true, silent = true})
+keymap.set("n", "<leader>bd", ":bdelete<CR>", {noremap = true, silent = true})
 
 
 keymap.set("t", "<Esc>", "<C-\\><C-n>", {noremap = true, silent = true})
