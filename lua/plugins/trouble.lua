e9b575d9b785e18c4c5ce2f4112e5ef7b30843ce
Caden LeCluyse <lecluyse20@gmail.com>
2024-04-16 19:11:50 -0500

    Added icons to trouble

--- lua/plugins/trouble.lua
+++ lua/plugins/trouble.lua
@@ -4,10 +4,6 @@ return {
       dependencies = { "nvim-tree/nvim-web-devicons" },
       lazy = false,
       config = function()
-         require("trouble").setup({
-            icons = false,
-         })
-
          vim.keymap.set("n", "<leader>tt", function()
             require("trouble").toggle()
          end)
