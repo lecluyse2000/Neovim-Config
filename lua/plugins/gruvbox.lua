254347bd737ff5c5c41a762472db763277f2b5fa
Caden LeCluyse <lecluyse20@gmail.com>
2024-04-18 01:34:26 -0500

    Changed status bar contrast

--- lua/plugins/gruvbox.lua
+++ lua/plugins/gruvbox.lua
@@ -6,6 +6,7 @@ return {
       vim.cmd('set background=dark')
       vim.g.gruvbox_material_background = 'hard'
       vim.g.gruvbox_material_foreground = 'mix'
+      vim.g.gruvbox_material_statusline_style = 'mix'
       vim.g.gruvbox_material_disable_italic_comment = 1
       vim.g.gruvbox_material_ui_contrast = 'high'
       vim.g.gruvbox_material_better_performance = 1
