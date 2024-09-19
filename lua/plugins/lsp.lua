f568842c97d37900c2a691b1a447e6af709cfb8b
lecluyse2000 <lecluyse20@gmail.com>
2024-09-19 16:40:49 -0500

    Removed sqls and hls

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -58,9 +58,7 @@ return {
       require('mason').setup({})
       require('mason-lspconfig').setup({
          ensure_installed = {
-            "hls",
             "lua_ls",
-            "sqls",
             "cmake",
             "autotools_ls",
             "clangd",
